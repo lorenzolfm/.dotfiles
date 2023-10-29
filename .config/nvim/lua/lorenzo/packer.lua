50d624913a9998d73e0a42b4af5c2f1ccbc8b5d4
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-10-29 16:55:10 -0300

    feat: updating telescope

--- .config/nvim/lua/lorenzo/packer.lua
+++ .config/nvim/lua/lorenzo/packer.lua
@@ -53,7 +53,7 @@ return require('packer').startup(function(use)
     use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 

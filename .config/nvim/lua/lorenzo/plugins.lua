9ec193a666dc70dd4fb8dd0b6221e979f4cbeb5d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-11-09 19:20:27 -0300

    organizing plugins

--- .config/nvim/lua/lorenzo/plugins.lua
+++ .config/nvim/lua/lorenzo/plugins.lua
@@ -27,26 +27,22 @@ local plugins = {
     'windwp/nvim-autopairs',
     'ellisonleao/carbon-now.nvim',
     'kristijanhusak/vim-dadbod-completion',
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'L3MON4D3/LuaSnip',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate'
+    },
     {
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' },   -- Optional
-            { 'hrsh7th/cmp-path' },     -- Optional
-            { 'hrsh7th/cmp-cmdline' },  -- Optional
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' }, -- Required
-        }
+        branch = 'v3.x'
     },
     {
         'nvim-telescope/telescope.nvim',

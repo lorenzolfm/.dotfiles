89e94bd08200f122d3e2d95e389bda6d41a47904
Lorenzo <maturanolorenzo@gmail.com>
2025-02-16 15:56:27 -0300

    feat(nvim): using rustacean instead of rust-tools

--- .config/nvim/lua/config/plugins/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lspconfig.lua
@@ -53,11 +53,9 @@ return {
 
         local servers = {
             bashls = { single_file_support = true, capabilities = capabilities },
-            -- jq = { single_file_support = true, capabilities = capabilities },
             lua_ls = { capabilities = capabilities },
             nil_ls = { single_file_support = true, capabilities = capabilities },
             protols = { single_file_support = true, capabilities = capabilities },
-            rust_analyzer = { capabilities = capabilities },
             svelte = { single_file_support = true, capabilities = capabilities },
             yamlls = { single_file_support = true, capabilities = capabilities },
         };

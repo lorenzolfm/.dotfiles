e63087da7d0f48e2c976d9305143ee4ebeacd3ac
Lorenzo <maturanolorenzo@gmail.com>
2025-02-16 10:30:03 -0300

    feat: oil, vim-fugitive, vim-startify

---
+++ .config/nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+    "stevearc/oil.nvim",
+    config = function()
+        local oil = require("oil");
+
+        oil.setup ({
+            view_options = {
+                show_hidden = true
+            }
+        })
+
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+}

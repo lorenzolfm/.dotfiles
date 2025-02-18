d485cbf8053ec3a35a748d739a9dd06d855f5683
Lorenzo <maturanolorenzo@gmail.com>
2025-02-18 13:50:07 -0300

    feat(nvim): fugitive config

--- .config/nvim/lua/config/plugins/fugitive.lua
+++ .config/nvim/lua/config/plugins/fugitive.lua
@@ -1,3 +1,9 @@
 return {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set('n', '<leader>gl', ':Git log --oneline<CR>')
+        vim.keymap.set('n', '<leader>gC', ':Git commit<CR>')
+        vim.keymap.set('n', '<leader>gr', ':Git rebase -i ')
+        vim.keymap.set('n', '<leader>gS', ':Git<CR>')
+    end
 }

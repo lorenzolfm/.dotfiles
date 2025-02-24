a27c7cfef9563e9dc219d16e1047b15cff1e4730
Lorenzo <maturanolorenzo@gmail.com>
2025-02-24 20:47:36 -0300

    feat(nvim): use check to checkOnSave

--- .config/nvim/lua/config/plugins/rustacean.lua
+++ .config/nvim/lua/config/plugins/rustacean.lua
@@ -2,4 +2,17 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5",
     lazy = false,
+    config = function()
+        vim.g.rustaceanvim = {
+            server = {
+                default_settings = {
+                    ['rust-analyzer'] = {
+                        checkOnSave = {
+                            command = "check",
+                        },
+                    },
+                },
+            },
+        }
+    end,
 }

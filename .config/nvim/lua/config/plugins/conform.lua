4810c0fc1b624824447ed9c4bed38be7d4127fe2
Lorenzo <maturanolorenzo@gmail.com>
2025-02-24 20:49:25 -0300

    feat(nvim): conform and treesitter

---
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+    "stevearc/conform.nvim",
+    config = function()
+        local conform = require("conform");
+
+        conform.setup({
+            formatters_by_ft = {
+                sql = { "sql_formatter" },
+            },
+        })
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
+        })
+    end,
+}
--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -5,8 +5,8 @@ return {
         local treesitter = require("nvim-treesitter.configs")
 
         treesitter.setup({
-              -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-              ensure_installed = {
+            -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+            ensure_installed = {
                 "gitcommit",
                 "javascript",
                 "lua",
@@ -24,27 +24,27 @@ return {
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
 
-          -- Automatically install missing parsers when entering buffer
-          -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-          auto_install = true,
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
 
-              -- List of parsers to ignore installing (or "all")
-              ignore_install = {},
+            -- List of parsers to ignore installing (or "all")
+            ignore_install = {},
 
-              highlight = {
+            highlight = {
                 enable = true,
 
                 -- Disable treesitter on large files
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
+                --disable = function(lang, buf)
+                --local max_filesize = 100 * 1024 -- 100 KB
+                --local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                --if ok and stats and stats.size > max_filesize then
+                --return true
+                --end
+                --end,
 
                 additional_vim_regex_highlighting = false,
-              },
+            },
         })
 
         vim.api.nvim_command("set foldmethod=expr")

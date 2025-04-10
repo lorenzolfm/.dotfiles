9211040a6276b648d7ad7236a432c59aa32c4395
Lorenzo <maturanolorenzo@gmail.com>
2025-04-10 06:45:57 -0300

    feat(nvim): add yamlfmt

--- .config/nvim/lua/config/plugins/conform.lua
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
             formatters_by_ft = {
                 sql = { "sql_formatter" },
                 nix = { "nixfmt" },
+                yaml = { "yamlfmt" },
             },
         })
 

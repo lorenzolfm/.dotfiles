74255be83a5d62396c9947173cf3117dbf98df0b
Lorenzo <maturanolorenzo@gmail.com>
2025-07-08 17:12:42 -0300

    feat: avante + copilot setup

--- .config/nvim/lua/config/plugins/avante.lua
+++ .config/nvim/lua/config/plugins/avante.lua
@@ -22,7 +22,20 @@ return {
         behavior = {
             enable_token_counting = false,
         },
-        provider = "copilot",
+
+        provider = "claude",
+        providers = {
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-sonnet-4-20250514",
+                timeout = 30000, -- Timeout in milliseconds
+                extra_request_body = {
+                    temperature = 0.75,
+                    max_tokens = 20480,
+                },
+            },
+        },
+
     },
     dependencies = {
         "nvim-lua/plenary.nvim",

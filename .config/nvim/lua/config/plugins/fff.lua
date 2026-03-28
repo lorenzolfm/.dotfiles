f1266b8fc3a57e7cd6f736cb790e578e95afcfbd
Lorenzo <maturanolorenzo@gmail.com>
2026-03-28 05:17:57 -0300

    feat(nvim): making fff fw grep word work

--- .config/nvim/lua/config/plugins/fff.lua
+++ .config/nvim/lua/config/plugins/fff.lua
@@ -8,6 +8,6 @@ return {
         { "<leader>ff", function() require("fff").find_files() end, desc = "Find files (fff)" },
         { "<leader>fa", function() require("fff").find_files() end, desc = "Find all files (fff)" },
         { "<leader>fg", function() require("fff").live_grep() end, desc = "Live grep (fff)" },
-        { "<leader>fw", function() require("fff").live_grep() end, desc = "Grep string (fff)" },
+        { "<leader>fw", function() require("fff").live_grep({ query = vim.fn.expand("<cword>") }) end, desc = "Grep word under cursor (fff)" },
     },
 }

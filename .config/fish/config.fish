6bc0331e0440f660f3d5d190eece72fd7fb800ca
Lorenzo <maturanolorenzo@gmail.com>
2026-06-28 09:44:45 -0300

    feat(fish): show current kubernetes context on command line

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,20 @@ atuin init fish | source
 starship init fish | source
 zoxide init fish | source
 
+# Bind kubectl to a cluster based on directory, so the wrong cluster can't be
+# inherited between terminals. Unknown dirs unset KUBECONFIG → no silent default.
+function __kube_autoselect --on-variable PWD
+    switch $PWD
+        case '/home/lorenzo/Projects/misc/homelab' '/home/lorenzo/Projects/misc/homelab/*'
+            set -gx KUBECONFIG ~/.kube/homelab.yaml
+        case '/home/lorenzo/Projects/Work' '/home/lorenzo/Projects/Work/*'
+            set -gx KUBECONFIG ~/.kube/work.yaml
+        case '*'
+            set -e KUBECONFIG
+    end
+end
+__kube_autoselect
+
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
